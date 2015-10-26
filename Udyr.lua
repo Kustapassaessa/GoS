b8a3fbbbdcc5b295674845875851d3ab5b09bbdf
Kustapassaessa <gerardo.neto@ymail.com>
2015-10-26 04:16:54 -0300

    Update Udyr.lua

--- Udyr.lua
+++ Udyr.lua
@@ -1,42 +1,40 @@
---Udyr Plain and Simple by Merda
-PrintChat("Udyr Plain and Simple")
+if GetObjectName(GetMyHero()) ~= "Udyr" then return end
+require('Inspired')
 
---Menu
-UdyrMenu = Menu("Udyr", "Udyr")
-UdyrMenu:SubMenu("Combo", "Combo")
-UdyrMenu.Combo:Boolean("Q", "Use Q", true)
-UdyrMenu.Combo:Boolean("W", "Use W", true)
-UdyrMenu.Combo:Boolean("E", "Use E", true)
-UdyrMenu.Combo:Boolean("R", "Use R", true)
+local info = "Udyr Plain and Simple by Merda"
+textTable = {info}
+PrintChat(textTable[1])
 
---Drawings
-UdyrMenu:SubMenu("Drawings", "Drawings")
-UdyrMenu.Drawings:Boolean("R","Draw R", false)
+Udyr = MenuConfig("Plain and Simple", "Udyr")
 
-OnLoop(function(myHero)
+Udyr:Menu("Combo", "Combo")
+Udyr.Combo:Boolean("Q", "Use Q", true)
+Udyr.Combo:Boolean("W", "Use W", true)
+Udyr.Combo:Boolean("E", "Use E", true)
+Udyr.Combo:Boolean("R", "Use R", true)
 
+Udyr:Menu("Keys", "Keys")
+Udyr.Keys:Key("Combo", "Combo", 32)
+
+OnTick(function(myHero)
 local target = GetCurrentTarget()
 
---Combo
-if IOW:Mode() == "Combo" then
---Use Bear Stance
-			if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 1550) and UdyrMenu.Combo.E:Value() then
-		       CastSpell(_E)
-			end
---Use Tiger Stance
-			if CanUseSpell(myHero, _Q) == READY and GoS:ValidTarget(target, 125) and UdyrMenu.Combo.Q:Value() then
-			   CastSpell(_Q)
-			end
---Use Phoenix Stance
-			if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, 250) and UdyrMenu.Combo.R:Value() then
-			   CastSpell(_R)
-			end
---Use Turtle Stance
-			if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 125) and UdyrMenu.Combo.W:Value() then
-			   CastSpell(_W)
-			end
+	if IOW:Mode() == "Combo" then
+
+		if CanUseSpell(myHero,_E) == READY and ValidTarget(target, 1550) and Udyr.Combo.E:Value() then
+		   CastSpell(_E)
+		end
+		
+		if CanUseSpell(myHero,_Q) == READY and ValidTarget(target, 125) and Udyr.Combo.Q:Value() then
+		   CastSpell(_Q)
+		end
+		
+		if CanUseSpell(myHero,_R) == READY and ValidTarget(target, 125) and Udyr.Combo.R:Value() then
+		   CastSpell(_R)
+		end
+		
+		if CanUseSpell(myHero,_W) == READY and ValidTarget(target, 125) and Udyr.Combo.W:Value() then
+		   CastSpell(_W)
+		end
 end
-
-if UdyrMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_R)),3,100,0xff00ff00) end
-
 end)
