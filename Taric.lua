82f507efd02c9eadb7a25af2f90d1ca740078fa3
Kustapassaessa <gerardo.neto@ymail.com>
2015-10-26 20:05:24 -0300

    Update Taric.lua

--- Taric.lua
+++ Taric.lua
@@ -1,63 +1,52 @@
---Taric by Merda
-
+if GetObjectName(GetMyHero()) ~= "Taric" then return end
 require('Inspired')
-require('IOW')
+PrintChat("Taric Ruby of Death by Merda")
 
---Menu
-TaricMenu = Menu("Taric", "Taric")
-TaricMenu:SubMenu("Combo", "Combo")
-TaricMenu.Combo:Boolean("Q", "Use Q", true)
-TaricMenu.Combo:Boolean("W", "Use W", true)
-TaricMenu.Combo:Boolean("E", "Use E", true)
-TaricMenu.Combo:Boolean("R", "Use R", true)
+Taric = MenuConfig("Ruby of Death", "Taric")
 
---Healing
-TaricMenu:SubMenu("Healing", "Healing")
-TaricMenu.Healing:Boolean("Q", "Auto Q", true)
-TaricMenu.Healing:Slider("HealQ", "Q Heal At %", 60, 1, 100, 1)
-TaricMenu.Healing:Info("Sep", "")
+Taric:Menu("Combo", "Combo")
+Taric.Combo:Boolean("Q", "Use Q", true)
+Taric.Combo:Boolean("W", "Use W", true)
+Taric.Combo:Boolean("E", "Use E", true)
+Taric.Combo:Boolean("R", "Use R", true)
 
---Drawings
-TaricMenu:SubMenu("Drawings", "Drawings")
-TaricMenu.Drawings:Boolean("W","Draw W", true)
+Taric:SubMenu("Drawings", "Drawings")
+Taric.Drawings:Boolean("E", "Draw Stun Range", false)
 
-OnLoop(function(myHero)
+Taric:SubMenu("Healing", "Healing")
+Taric.Healing:Boolean("Q", "Auto Q", true)
+Taric.Healing:Slider("HealQ", "Q Heal At %", 60, 1, 100, 1)
+Taric.Healing:Info("Sep", "")
 
-local target = IOW:GetTarget()
-local myHero = GetMyHero()
+Taric:Menu("Keys", "Keys")
+Taric.Keys:Key("Combo", "Combo", 32)
 
---Combo
-if IOW:Mode() == "Combo" then
---Use Shatter
-			if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 340) and TaricMenu.Combo.W:Value() then
-			   CastSpell(_W)
-			end
---Use Radiance
-			if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, 340) and TaricMenu.Combo.R:Value() then
-               CastSpell(_R)
-			end
---Use Dazzle
-			if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target,GetCastRange(myHero,_E)) then
+OnTick(function(myHero)
+local target = GetCurrentTarget()
+
+	if IOW:Mode() == "Combo" then
+
+		if CanUseSpell(myHero,_E) == READY and ValidTarget(target, 625) and Taric.Combo.E:Value() then
 		   CastTargetSpell(target,_E)
-			end
-
+		end
+		
+		if CanUseSpell(myHero,_W) == READY and ValidTarget(target, 340) and Taric.Combo.W:Value() then
+		   CastSpell(_W)
+		end
+		
+		if CanUseSpell(myHero,_R) == READY and ValidTarget(target, 340) and Taric.Combo.R:Value() then
+		   CastSpell(_R)
+		end
 end
 
---Auto use Imbue
-if TaricMenu.Healing.Q:Value() then
-				for _, ally in pairs(GoS:GetAllyHeroes()) do
-						if (100 * GetCurrentHP(ally))/GetMaxHP(ally) <  TaricMenu.Healing.HealQ:Value() and
-							CanUseSpell(myHero, _Q) == READY and GoS:IsInDistance(ally, 750) then
+if Taric.Healing.Q:Value() then
+				for _, ally in pairs(GetAllyHeroes()) do
+						if (100 * GetCurrentHP(ally))/GetMaxHP(ally) <  Taric.Healing.HealQ:Value() and
+							CanUseSpell(myHero, _Q) == READY and IsInDistance(ally, 750) then
 								CastTargetSpell(ally, _Q);
                         end
                     end
                 end
 
-if TaricMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_W)),3,100,0xff00ff00) end
-
-
-    end)
-
-GoS:AddGapcloseEvent(_E, 610, true)
-
-	PrintChat("Taric Ruby of Death")
+if Taric.Drawings.E:Value() then DrawCircle(myHeroPos().x, myHeroPos().y, myHeroPos().z,(GetCastRange(myHero,_E)),3,100,0xff00ff00) end
+end)
