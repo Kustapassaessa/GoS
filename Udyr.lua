6c71e2ed2c7b5089ef18c2525193b03ad264614c
Kustapassaessa <gerardo.neto@ymail.com>
2015-11-20 02:55:23 -0300

    Update Udyr.lua

--- Udyr.lua
+++ Udyr.lua
@@ -1,9 +1,7 @@
 if GetObjectName(GetMyHero()) ~= "Udyr" then return end
 require('Inspired')
 
-local info = "Udyr Plain and Simple by Merda"
-textTable = {info}
-PrintChat(textTable[1])
+PrintChat("Udyr Plain and Simple by Merda")
 
 Udyr = MenuConfig("Plain and Simple", "Udyr")
 
@@ -20,19 +18,19 @@ OnTick(function(myHero)
 local target = GetCurrentTarget()
 
 	if IOW:Mode() == "Combo" then
-
+	
 		if CanUseSpell(myHero,_E) == READY and ValidTarget(target, 1550) and Udyr.Combo.E:Value() then
 		   CastSpell(_E)
 		end
 		
-		if CanUseSpell(myHero,_Q) == READY and ValidTarget(target, 125) and Udyr.Combo.Q:Value() then
+		if CanUseSpell(myHero,_Q) == READY and ValidTarget(target, 130) and Udyr.Combo.Q:Value() then
 		   CastSpell(_Q)
 		end
 		
 		if CanUseSpell(myHero,_R) == READY and ValidTarget(target, 125) and Udyr.Combo.R:Value() then
 		   CastSpell(_R)
 		end
-		
+		local WPred = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),1700,250,900,50,false,true)
 		if CanUseSpell(myHero,_W) == READY and ValidTarget(target, 125) and Udyr.Combo.W:Value() then
 		   CastSpell(_W)
 		end
